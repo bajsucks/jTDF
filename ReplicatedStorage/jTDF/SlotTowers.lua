7a37b3185134327776ecb4b5ad55fccd0bf15b1a
baj <166956883+bajsucks@users.noreply.github.com>
2025-09-26 17:48:43 +0300

    Updated ReplicatedStorage/jTDF/SlotTowers.lua

--- ReplicatedStorage/jTDF/SlotTowers.lua
+++ ReplicatedStorage/jTDF/SlotTowers.lua
@@ -6,43 +6,41 @@
 	Author: baj (@artembon)
 	Description: Holds read-only information about each tower.
 	
-	SlotTowers CANNOT be changed at runtime
+	SlotTowers shouldn't be changed at runtime
 	Consider using Unit's StatusEffect for dynamic stat changes.
 ]]
 
-type Tower = {
+export type Stats = {
+	["Cost"]: number,
+	["Range"]: number?, -- studs
+	["Damage"]: number,
+	["FireRate"]: number, -- seconds
+	["FireFunction"]: (string) -> (boolean)?
+	-- FireFunction receives TargetID
+	-- When true is returned, firerate will be applied
+	-- tip: You can intentionally yield this function to wait BEFORE firerate applies
+}
+
+export type Tower = {
 	["Name"]: string,
 	["Upgrades"]: {
-		[number]: {
-			["Cost"]: number,
-			["Stats"]: {
-				["Range"]: number?, -- studs
-				["Damage"]: number,
-				["FireRate"]: number, -- seconds
-				["FireFunction"]: (string) -> (boolean)?
-				-- FireFunction receives TargetID
-				-- When true is returned, firerate will be applied
-				-- tip: You can intentionally yield this function to wait BEFORE firerate applies
-			}
-		}
+		[number]: Stats
 	}
 }
 
 local SlotTowers:{[string]: Tower} = table.freeze {
-	--["Pistol Guy"] = {
-	--	["Name"] = "Pistol Guy",
-	--	["Upgrades"] = {
-	--		[1] = {
-	--			["Cost"] = 10,
-	--			["Stats"] = {
-	--				["Range"] = 1,
-	--				["Damage"] = 1,
-	--				["FireRate"] = 1,
-	--				["FireFunction"] = nil
-	--			}
-	--		},
-	--	}
-	--}
+	["Pistol Guy"] = {
+		["Name"] = "Pistol Guy",
+		["Upgrades"] = {
+			[1] = {
+				["Cost"] = 10,
+				["Range"] = 1,
+				["Damage"] = 1,
+				["FireRate"] = 1,
+				["FireFunction"] = nil
+			},
+		}
+	}
 }
 
 return SlotTowers
