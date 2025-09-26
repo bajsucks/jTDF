5ccd257f27881c3c7970dbe24fec931a9ab777a9
baj <166956883+bajsucks@users.noreply.github.com>
2025-09-26 17:50:28 +0300

    Updated ReplicatedStorage/jTDF/Enemies.lua

--- ReplicatedStorage/jTDF/Enemies.lua
+++ ReplicatedStorage/jTDF/Enemies.lua
@@ -1,6 +1,14 @@
 -- @ScriptType: ModuleScript
+--[[
+	Enemies module
+	Created: 9/26/2025
+	Last updated: 9/26/2025
+	Author: baj (@artembon)
+	Description: Holds information about enemies.
+]]
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-type Enemy = {
+export type Enemy = {
 	["Name"]: string,
 	["BaseHealth"]: number,
 	["BaseSpeed"]: number,
@@ -8,12 +16,12 @@ type Enemy = {
 }
 
 local Enemies: {[string]: Enemy} = {
-	--["Green Zombie"] = {
-	--	["Name"] = "",
-	--	["BaseHealth"] = 1,
-	--	["BaseSpeed"] = 1,
-	--	["Model"] = ReplicatedStorage.Enemies["Green Zombie"]
-	--}
+	["Green Zombie"] = {
+		["Name"] = "",
+		["BaseHealth"] = 1,
+		["BaseSpeed"] = 1,
+		["Model"] = ReplicatedStorage.Enemies["Green Zombie"]
+	}
 }
 
 return Enemies
