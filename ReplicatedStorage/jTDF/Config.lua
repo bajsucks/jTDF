159b0e838b15ecff8381a585528ea30ba75e2ec0
baj <166956883+bajsucks@users.noreply.github.com>
2025-09-26 17:41:43 +0300

    Updated ReplicatedStorage/jTDF/Config.lua

--- ReplicatedStorage/jTDF/Config.lua
+++ ReplicatedStorage/jTDF/Config.lua
@@ -1,4 +1,13 @@
 -- @ScriptType: ModuleScript
+--[[
+	Config module
+	Created: 9/26/2025
+	Last updated: 9/26/2025
+	Author: baj (@artembon)
+	Description: Holds configurations for the jTDF module.
+	
+]]
+
 local Config = {
 	
 }
