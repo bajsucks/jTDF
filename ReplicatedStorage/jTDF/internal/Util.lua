79d3c089707353a4070cbb861f03eb1c5e6f72c1
baj <166956883+bajsucks@users.noreply.github.com>
2025-10-05 10:49:40 +0300

    Updated ReplicatedStorage/jTDF/internal/Util.lua

--- ReplicatedStorage/jTDF/internal/Util.lua
+++ ReplicatedStorage/jTDF/internal/Util.lua
@@ -1,4 +1,11 @@
 -- @ScriptType: ModuleScript
+--[[
+	Util module
+	Created: 9/26/2025
+	Last updated: 10/5/2025
+	Author: baj (@artembon)
+	Description: Utility functions for jTDF
+]]
 local Util = {}
 
 local Signal = require(script.Parent.Signal)
@@ -8,5 +15,18 @@ function Util.signalfor(t:{}, ind:{string})
 		t[v] = Signal()
 	end
 end
+
+
+function Util.BulkPivotTo(models:{PVInstance}, CFrames:{CFrame}, mode:Enum.BulkMoveMode)
+	local newCFrames = {}
+	local newModels = {}
+	for i, v in models do
+		local pivotOffset = v:GetPivot():Inverse() * v.PrimaryPart.CFrame
+		table.insert(newModels, v.PrimaryPart)
+		table.insert(newCFrames, CFrames[i] * pivotOffset)
+	end
+	workspace:BulkMoveTo(newModels, newCFrames, mode)
+end
+
 
 return Util
