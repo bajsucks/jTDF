a9e59b60435476f4067afb19b713db4deb9b90bd
baj <166956883+bajsucks@users.noreply.github.com>
2025-10-12 19:04:08 +0300

    Updated ReplicatedStorage/jTDF/Parallel/Waypoints.lua

--- ReplicatedStorage/jTDF/Parallel/Waypoints.lua
+++ ReplicatedStorage/jTDF/Parallel/Waypoints.lua
@@ -11,21 +11,37 @@
 
 local actor = script:GetActor()
 local CollectionService = game:GetService("CollectionService")
+local SharedTableRegistry = game:GetService("SharedTableRegistry")
 local jtdfmodule = CollectionService:GetTagged("_JTDFMODULE")[1]
 
-local function GetProgress(Enemy)
-	local pos1 = Enemy.CurrentPath.WorldPosition
-	local pos2 = Enemy.NextPath.WorldPosition
-	local PathLength = (pos1 - pos2).Magnitude
-	local TimeSpent = workspace:GetServerTimeNow() - Enemy.StartTime
+local function GetProgress(Enemy, t)
+	local pos1: vector = Enemy.CurrentPath
+	local pos2: vector = Enemy.NextPath
+	local PathLength = (pos1 - pos2).magnitude
+	local TimeSpent = t - Enemy.StartTime
 	local DistanceCovered = TimeSpent * Enemy.Speed
 	local Progress = DistanceCovered / PathLength
 	return Progress
 end
 actor:BindToMessageParallel("UpdateEnemyWaypoint", function(batch:{{}})
+	--debug.profilebegin("Getting SharedTable")
+	--local EnemiesProgress = SharedTableRegistry:GetSharedTable("EnemiesProgress")
+	--debug.profileend()
+	local t = workspace:GetServerTimeNow()
+	local NewProgress = {}
 	for i, self in batch do
-		if GetProgress(self) >= 1 then
-			jtdfmodule.WaypointReply:Fire(tostring(self.EnemyID))
+		--debug.profilebegin("Calculation")
+		--local p = GetProgress(self, t)
+		--debug.profileend()
+		debug.profilebegin("Calculation + Table")
+		local p = GetProgress(self, t)
+		NewProgress[self.EnemyID] = p
+		debug.profileend()
+		if p >= 1 then
+			jtdfmodule.WaypointReply:Fire(self.EnemyID)
 		end
 	end
+	debug.profilebegin("Sharedtable")
+	SharedTableRegistry:SetSharedTable("EnemiesProgress", SharedTable.new(NewProgress))
+	debug.profileend()
 end)
